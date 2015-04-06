68aa28610bbc992f4ecc0630662ab7751704138d
agoldvarg <agoldvarg@gmail.com>
2015-04-06 17:08:37 -0400

    add user model factories and spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
+  
   factory :user do
-    
+    email {Faker::Internet.email}
+    password {Faker::Internet.password}
+    password_confirmation { |u| u.password }
+    first_name {Faker::Name.first_name}
+    last_name {Faker::Name.last_name}
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has a valid factory" do
+    expect(create(:user)).to be_valid
+  end
+
+  it "is invalid without a first name" do
+    user = build(:user, :first_name => nil)
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without a last name" do
+    user = build(:user, :last_name => nil)
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without an email address" do
+    user = build(:user, :email => nil)
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without a valid email format" do
+    user = build(:user, :email => "bad--email@bad")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid if email adress is not unique" do
+    user1 = create(:user, :email => "test@test.com")
+    user2 = build(:user, :email => "test@test.com")
+    expect(user2).not_to be_valid
+  end
+
+  it "is invalid with a non-matching password confirmation" do
+    user = build(:user, :password => "ponies13", :password_confirmation => "13ponies")
+    expect(user).not_to be_valid
+  end
+
 end
