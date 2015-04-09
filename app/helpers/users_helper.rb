ab263d543c728c296472cbbcf8e5bffd042e75f0
Seiji Naganuma <Snaganumas@gmail.com>
2015-04-08 20:14:13 -0400

    add styling to user show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,36 @@ module UsersHelper
   def disabled
     "disabled" if current_user.available_user_languages.count < 1
   end
+
+  def language_icon(language)
+    case language.name
+    when "Ruby"
+      "ruby"
+    when "JavaScript"
+      "javascript"
+    end
+  end
+
+  def language_color(language)
+    case language.name
+    when "Ruby"
+      "#E74C3C"
+    when "JavaScript"
+      "#F1C40F"
+    end
+  end
+
+  def language_completion(language)
+    completed = completed_count(language).to_f
+    total = total_count(language).to_f
+    (completed / total * 100)
+  end
+
+  def completed_count(language)
+    current_user.completed_problems(language).count
+  end
+
+  def total_count(language)
+    LanguageProblem.where(:language => language).count
+  end
 end
