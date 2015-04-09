1a9b1a2429fe331379f4a498cd8b200a7f71a779
Seiji Naganuma <Snaganumas@gmail.com>
2015-04-09 14:26:48 -0400

    post show page styling. comments section

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,8 @@
 module CommentsHelper
+
+  def delete_comment_link(user, comment)
+    if comment.user == user
+      link_to "Delete", post_comment_path(comment.post, comment), :method => :delete, :remote => true
+    end
+  end
 end
