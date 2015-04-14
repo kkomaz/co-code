a920ddcd3a22a19046711715b1caf396cf35dbd8
agoldvarg <agoldvarg@gmail.com>
2015-04-14 14:54:14 -0400

    add ability to delete posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,10 @@ module PostsHelper
   def comment_count(post)
     post.comments.count > 0 ? "#{post.comments.count} comments" : "no comments :("
   end
+
+  def delete_post_link(user, post)
+    if post.user == user
+      link_to "Delete Post", destroy_problem_post_path(post), :method => :delete, :remote => true
+    end
+  end
 end
