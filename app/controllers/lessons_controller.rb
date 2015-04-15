a3398e019f8984f7d21b039ef1dbc999ffeb3191
agoldvarg <agoldvarg@gmail.com>
2015-04-15 17:32:56 -0400

    add lessons controller

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,37 @@
+class LessonsController < ApplicationController
+
+  def index
+  end
+  
+  def new
+    @language = Language.find(params[:language_id])
+    @problems = Problem.all
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    @lesson.host = current_user
+    @language_problem = LanguageProblem.find_language_problem(params[:language_id], params[:problem_id])
+    @lesson.room.build(:title => params[:lesson][:description], :language_problem => @language_problem)
+    if @lesson.save
+      # redirect_to
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+    def lesson_params
+      params.require(:lesson).permit(:description, :schedule)
+    end
+
+end
