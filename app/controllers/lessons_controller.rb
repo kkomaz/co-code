9fccff81a16cd884b0803340d53c5837b38ddbc9
agoldvarg <agoldvarg@gmail.com>
2015-04-15 18:28:52 -0400

    create invitiations on lesson create action

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -12,11 +12,12 @@ class LessonsController < ApplicationController
   def create
     binding.pry
     @lesson = Lesson.new(lesson_params)
-    @lesson.host = current_user
+    @invitees = params[:user][:id]
     @language_problem = LanguageProblem.find_language_problem(params[:language_id], params[:problem_id])
     @lesson.build_room(:title => params[:lesson][:description], :language_problem => @language_problem)
     if @lesson.save
-      # redirect_to
+      Invitation.create_invitations(@lesson, @invitees)
+      redirect_to user_progress_path(current_user)
     else
       render 'new'
     end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,11 @@
 class Invitation < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :lesson
+
+  def self.create_invitations(lesson, invitees)
+    invitees.each do |invitee|
+      Invitation.create(:lesson => lesson, :user => invitee)
+    end
+  end
+  
 end
