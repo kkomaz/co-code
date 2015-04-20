1fd388334460e3177716958c438973544c4860fd
kkomaz <leeajl006@gmail.com>
2015-04-20 14:24:24 -0400

    UserProgress model refactored

---
+++ app/models/concerns/user_progresses/problem_statuses.rb
@@ -0,0 +1,28 @@
+module Concerns
+  module UserProgresses
+    module ProblemStatuses
+      extend ActiveSupport::Concern
+
+      module ClassMethods
+
+        def set_first_problem_as_current(user, language_id)
+          problem = Problem.first_problem
+          language = Language.find(language_id)
+          user_progress = progresses(language,problem).where(:user => user).first
+          user_progress.update(:status => 1)
+        end
+
+        def problem_users(language, problem, user, status)
+          user_ids = progresses(language,problem).where(:status => status).pluck(:user_id)
+          User.where(:id => user_ids)
+        end
+
+        def progresses(language, problem)
+          UserProgress.joins(:language_problem).
+          where(:language_problems => {:language_id => language, :problem_id => problem})
+        end
+
+      end
+    end
+  end
+end
--- app/models/user_progress.rb
+++ app/models/user_progress.rb
@@ -1,4 +1,6 @@
 class UserProgress < ActiveRecord::Base
+  include Concerns::UserProgresses::ProblemStatuses
+
   belongs_to  :user
   belongs_to  :language_problem
   delegate :language, :to => :language_problem
@@ -10,34 +12,7 @@ class UserProgress < ActiveRecord::Base
 
   validates_uniqueness_of :user_id, :scope => :language_problem_id
 
-  def self.find_or_build_user_progress(language_problem, user)
-    user_progress = UserProgress.find_or_create_by(:user => user, :language_problem => language_problem)
-    user_progress.status = 1
-    user_progress
-  end
-
   def self.progress_for_user(user, language_problem)
-    self.where({:user => user, :language_problem => language_problem})[0]
-  end
-
-  def self.set_first_problem_as_current(user, language_id)
-    problem = Problem.first_problem
-    language_problem = LanguageProblem.find_language_problem(language_id, problem.id)
-    user_progress = self.where({:user => user, :language_problem => language_problem}).first
-    user_progress.update(:status => 1)
-  end
-
-  def self.current_problem_users(language, problem, user)
-    language_problem = LanguageProblem.where(:language_id => language.id, :problem_id => problem.id)
-    current_user_ids = UserProgress.where(:language_problem => language_problem, :status => 1).pluck(:user_id)
-    users = User.where(:id => current_user_ids)
-    # .where.not(:id => user.id)
-  end
-
-  def self.finished_problem_users(language, problem, user)
-    language_problem = LanguageProblem.where(:language_id => language.id, :problem_id => problem.id)
-    current_user_ids = UserProgress.where(:language_problem => language_problem, :status => 2).pluck(:user_id)
-    users = User.where(:id => current_user_ids)
-    # .where.not(:id => user.id)
+    self.where({:user => user, :language_problem => language_problem}).first
   end
 end
