db571d2f682d35bdb6b28f0bee3770ee2404d4e9
agoldvarg <agoldvarg@gmail.com>
2015-05-10 19:58:06 -0400

    small changes to comment_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,26 +3,30 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   describe "Comment validations" do
     it "has a valid factory" do
-      expect(create(:comment)).to be_valid
+      expect(build(:comment)).to be_valid
     end
 
     it "is valid with a user_id, post_id, and content" do
-      comment = Comment.new(user_id: 1, post_id: 1, content: "Comment content")
+      comment = Comment.new(:user_id => 1, :post_id  => 1, :content => "Comment content")
+
       expect(comment).to be_valid
     end
 
     it "is invalid without content" do
       comment = build(:comment, :content => nil)
+
       expect(comment).not_to be_valid
     end
 
     it "is invalid without a post id" do
       comment = build(:comment, :post => nil)
+
       expect(comment).not_to be_valid
     end
 
     it "is invalid without a user id" do
       comment = build(:comment, :user => nil)
+
       expect(comment).not_to be_valid
     end
   end
