154addc82806643f586253d13562ea6d28e19b37
agoldvarg <agoldvarg@gmail.com>
2015-05-13 19:59:36 -0400

    refactor comment model specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,33 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
+describe Comment do
+
   describe "Comment validations" do
     it "has a valid factory" do
       expect(build(:comment)).to be_valid
     end
 
-    it "is valid with a user_id, post_id, and content" do
-      comment = Comment.new(:user_id => 1, :post_id  => 1, :content => "Comment content")
-
-      expect(comment).to be_valid
-    end
-
     it "is invalid without content" do
       comment = build(:comment, :content => nil)
+      comment.valid?
 
-      expect(comment).not_to be_valid
+      expect(comment.errors[:content]).to include("can't be blank")
     end
 
     it "is invalid without a post id" do
-      comment = build(:comment, :post => nil)
+      comment = build(:comment, :post_id => nil)
+      comment.valid?
 
-      expect(comment).not_to be_valid
+      expect(comment.errors[:post_id]).to include("can't be blank")
     end
 
     it "is invalid without a user id" do
-      comment = build(:comment, :user => nil)
+      comment = build(:comment, :user_id => nil)
+      comment.valid?
 
-      expect(comment).not_to be_valid
+      expect(comment.errors[:user_id]).to include("can't be blank")
     end
   end
 end
