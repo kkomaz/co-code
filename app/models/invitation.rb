9b0610304cf4833515f8027e48dd11ded3da8c17
agoldvarg <agoldvarg@gmail.com>
2015-05-13 23:18:42 -0400

    add validations to invitaion model and add invitation specs

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,6 +2,8 @@ class Invitation < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :lesson
 
+  validates :user, :lesson, presence: true
+
   def self.create_invitations(lesson, invitees)
     invitees.each do |invitee|
       if invitee != ""
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 describe Invitation, type: :model do
-  describe 'Invitation validations' do
-    it "has a valid factory" do
-      expect(build(:invitation)).to be_valid
-    end
+
+  it "has a valid factory" do
+    expect(build(:invitation)).to be_valid
+  end
+
+  it "is invalid without a lesson" do
+    invitation = build(:invitation, lesson: nil)
+    invitation.valid?
+
+    expect(invitation.errors[:lesson]).to include("can't be blank")
+  end
+
+  it "is invalid without a user" do
+    invitation = build(:invitation, user: nil)
+    invitation.valid?
+
+    expect(invitation.errors[:user]).to include("can't be blank")
   end
 end
